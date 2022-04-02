5bbf88cef67237852eadf9f268ae0e1e8465191d
eskov-ea <70005887+eskov-ea@users.noreply.github.com>
2022-04-02 13:24:24 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('push', e => {
   const data = e.data.json();
   console.log('SW data is ', data);
-  console.log("actions is: ", data.actions);
+console.log("actions is: ", data.actions);
   self.registration.showNotification(data.title, {
       body: data.body,
       icon: "favicon.ico",
