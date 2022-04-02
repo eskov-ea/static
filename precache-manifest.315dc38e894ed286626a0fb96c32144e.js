7a7ac1c67eb872c09b061a18dd59e86f129eedb3
eskov-ea <70005887+eskov-ea@users.noreply.github.com>
2022-04-02 11:47:44 +1000

    Add files via upload

---
+++ precache-manifest.315dc38e894ed286626a0fb96c32144e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50de8c58f971887dece8",
+    "url": "/static/css/app.6553a7e3.css"
+  },
+  {
+    "revision": "390a77bba9d17d83ce3a",
+    "url": "/static/css/chunk-vendors.bf607588.css"
+  },
+  {
+    "revision": "b0dc21936e27b98d6c65bd72dd705c65",
+    "url": "/static/fonts/bootstrap-icons.b0dc2193.woff"
+  },
+  {
+    "revision": "c5787b4ab55835580294696c3cbcc521",
+    "url": "/static/fonts/bootstrap-icons.c5787b4a.woff2"
+  },
+  {
+    "revision": "442931e647c0fad78d2d0809c7983688",
+    "url": "/static/index.html"
+  },
+  {
+    "revision": "7a59d89a79cbd7b1c5b3",
+    "url": "/static/js/about.2fd402c9.js"
+  },
+  {
+    "revision": "50de8c58f971887dece8",
+    "url": "/static/js/app.3a57e9db.js"
+  },
+  {
+    "revision": "390a77bba9d17d83ce3a",
+    "url": "/static/js/chunk-vendors.559380b6.js"
+  },
+  {
+    "revision": "a73b1ff9a04e7c7ebe092605a862a8f8",
+    "url": "/static/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/static/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,11 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
-
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-importScripts(
-  "/static/precache-manifest.ac32f253fe71f1293e5f41c2ab8ef88e.js"
-);
-
-workbox.core.setCacheNameDetails({prefix: "web-notifications"});
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
 self.addEventListener('push', e => {
   const data = e.data.json();
-  console.log('SW data is ', data)
+  console.log('SW data is ', data);
+  console.log("actions is: ", data.actions);
   self.registration.showNotification(data.title, {
       body: data.body,
-      icon: 'favicon.png',
-      image: data.image
+      icon: "favicon.ico",
+      image: data.image,
+
   });
-
 });
-
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
