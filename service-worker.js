a6ff56a4a6ebd57ed795ef459c65404544aea263
eskov-ea <70005887+eskov-ea@users.noreply.github.com>
2022-04-03 12:18:10 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,30 @@
-self.addEventListener('push', e => {
+const visible = true;
+
+self.addEventListener("push", (e) => {
   const data = e.data.json();
-  console.log('SW data is ', data);
+  console.log("SW data is ", data);
   console.log("actions is: ", data.actions);
-  const actions = [{action: 'reply', title: "Reply"}];
+  const actions = [{ action: "reply", title: "Reply" }];
   self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: "favicon.ico",
-      image: data.image,
-      actions: actions
+    body: data.body,
+    icon: "favicon.ico",
+    image: data.image,
+    actions: actions,
   });
 });
+
+self.addEventListener("message", (e) => {
+
+});
+
+self.addEventListener(
+  "notificationclick",
+  function (event) {
+    event.notification.close();
+
+    if (hidden) {
+      clients.openWindow("/static/");
+    }
+  },
+  false
+);
