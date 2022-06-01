7c5c8dadde1eaf9433e429ce1e2b9c76fcc298d5
eskov-ea <70005887+eskov-ea@users.noreply.github.com>
2022-06-01 23:02:28 +1000

    Add files via upload

---
+++ precache-manifest.8f00c3488b3994b4e359d9dcd14754bd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689c2fe1c6419a26e0e0",
+    "url": "/static/css/app.5a2fd13d.css"
+  },
+  {
+    "revision": "0f577cb0001d19a99a8b",
+    "url": "/static/css/chunk-vendors.bf607588.css"
+  },
+  {
+    "revision": "b0dc21936e27b98d6c65bd72dd705c65",
+    "url": "/static/fonts/bootstrap-icons.b0dc2193.woff"
+  },
+  {
+    "revision": "c5787b4ab55835580294696c3cbcc521",
+    "url": "/static/fonts/bootstrap-icons.c5787b4a.woff2"
+  },
+  {
+    "revision": "07a99ae1917ea2aac60ee78be0428934",
+    "url": "/static/index.html"
+  },
+  {
+    "revision": "00eb453d556c4e301fc2",
+    "url": "/static/js/about.e34ee262.js"
+  },
+  {
+    "revision": "689c2fe1c6419a26e0e0",
+    "url": "/static/js/app.df32caed.js"
+  },
+  {
+    "revision": "0f577cb0001d19a99a8b",
+    "url": "/static/js/chunk-vendors.e5c1825c.js"
+  },
+  {
+    "revision": "a73b1ff9a04e7c7ebe092605a862a8f8",
+    "url": "/static/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/static/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -13,16 +13,36 @@ self.addEventListener("push", (e) => {
   });
 });
 
+self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
+
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+
 self.addEventListener("message", (e) => {
   console.log(e.data);
   visible = e.data;
 });
-self.addEventListener("notificationclick", function (event) {
+self.addEventListener("notificationclick", async function (event) {
     event.notification.close();
-
-    if (!visible) {
-      clients.openWindow("/static/");
-    }
+    let url = "https://web-notifications.ru/static/";
+    event.waitUntil(
+    clients.matchAll({type: 'window', includeUncontrolled: true,}).then( windowClients => {
+            // Check if there is already a window/tab open with the target URL
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                // If so, just focus it.
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            // If not, then open the target URL in a new window/tab.
+            if (clients.openWindow) {
+                return clients.openWindow("/static/");
+            }
+        })
+    )
+    // if (!visible) {
+    //  clients.openWindow("/static/");
+    // }
   },
   false
 );
