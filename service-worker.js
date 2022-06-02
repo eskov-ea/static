2ab1277072e3c422585aa07ef7fc255f2e651899
eskov-ea <70005887+eskov-ea@users.noreply.github.com>
2022-06-02 22:36:12 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,48 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+let visible = true;
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-importScripts(
-  "/static/precache-manifest.e75ccad960426cb379f18e3d43faac91.js"
-);
-
-workbox.core.setCacheNameDetails({prefix: "web-notifications"});
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
+self.addEventListener("push", (e) => {
+  const data = e.data.json();
+  console.log("SW data is ", data);
+  console.log("actions is: ", data.actions);
+  const actions = [{ action: "reply", title: "Reply" }];
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: "favicon.ico",
+    image: data.image,
+    actions: actions,
+  });
 });
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
+
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+
+self.addEventListener("message", (e) => {
+  console.log(e.data);
+  visible = e.data;
+});
+self.addEventListener("notificationclick", async function (event) {
+    event.notification.close();
+    let url = "https://web-notifications.ru/static/";
+    event.waitUntil(
+    clients.matchAll({type: 'window', includeUncontrolled: true,}).then( windowClients => {
+            // Check if there is already a window/tab open with the target URL
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                // If so, just focus it.
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            // If not, then open the target URL in a new window/tab.
+            if (clients.openWindow) {
+                return clients.openWindow("/static/");
+            }
+        })
+    )
+    // if (!visible) {
+    //  clients.openWindow("/static/");
+    // }
+  },
+  false
+);
